0b9286ea309725bf4c7b209dca9c29451fd4508c
Maxillas <maksanchutin@mail.ru>
2024-02-15 21:49:43 +0500

    15.02 21:49

---
+++ Lesson1/answer.sql
@@ -0,0 +1,11 @@
+Employee -> EmployeeTerritories <- Territories = Многие ко многим
+Territories -> Region = Один ко многим
+Employee -> Orders = Один ко многим
+Orders -> Customer = Один ко многим
+Orders -> Shippers = Один ко многим
+Orders -> OrdersDetails <- Product = Многие ко многим
+Product -> Supliers = Один ко многим
+Product -> Categories = Один ко многим
+
+Случаи многие ко многим относятся именно к этому типу поскольку в связке используется три таблицы, 
+средняя (EmployeeTerritories и OrderDetails) является связующей и хранит пары EmployeeID - TerritoryID и OrderID - ProductID 

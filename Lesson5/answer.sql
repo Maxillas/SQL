9cb40347aa88c02549ebd577ad403a1385f383b2
Maxillas <maksanchutin@mail.ru>
2024-02-24 11:58:47 +0500

    Add correct answer and reflection

--- Lesson5/answer.sql
+++ Lesson5/answer.sql
@@ -1,5 +1,11 @@
-SELECT ContactType FROM Contacts
+SELECT ContactType, COUNT(ContactType) FROM Contacts
 GROUP BY ContactType
 
-SELECT AVG(UnitPrice) AS AVG_UNIT_PRICE FROM Products 
+SELECT CategoryID, AVG(UnitPrice) AS AVG_UNIT_PRICE FROM Products 
 GROUP BY CategoryID
+ORDER BY AVG_UNIT_PRICE
+
+/*Рефлексия: 
+1. В первом задании не понял смысла слова "агрегация" и сделал просто группировку по полю contactType. Добавил Счетчик
+2. Во втором задании не вывел категорию (CategoyID), это больше для наглядности запроса должно быть
+Также не сделал сортировку по возрастанию*/

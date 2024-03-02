7cefbb5502c8aaa37d945e42ce2977296914b661
Maxillas <maksanchutin@mail.ru>
2024-03-02 18:50:54 +0500

    Add reflection

--- Lesson8/answer.sql
+++ Lesson8/answer.sql
@@ -1,6 +1,9 @@
 SELECT t1.CustomerID, t2.CustomerID, t2.Region
 FROM Customers t1, Customers t2
-WHERE t1.CustomerID != t2.CustomerID AND (t2.Region is NULL)
+WHERE t1.CustomerID != t2.CustomerID AND (t2.Region is NULL) AND t1.Region is NULL
+
+Не было сравнения региона из таблицы 1, добавил, но не уверен что это нужно, 
+значения ведь одинаковые...
 
 SELECT * FROM Orders
 WHERE CustomerID = ANY 
@@ -8,7 +11,13 @@ WHERE CustomerID = ANY
  FROM Customers 
  WHERE Region IS NOT NULL)
 
+По сути все совпадает, но отличие в том, что у меня в запросе выбирается вся таблица, 
+считаю это более корректным, т.к. в задании нужно вывести список заказов, в моем понимании -
+это список со всеми параметрами.
+
 SELECT * FROM Orders
 WHERE Freight > ALL 
 (SELECT UnitPrice 
  FROM Products)
+
+Аналогично предыдущей задаче

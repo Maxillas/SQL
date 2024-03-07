e586e88321359973efeee0dd273d8d91999a8b9c
Maxillas <maksanchutin@mail.ru>
2024-03-07 20:18:29 +0500

    Lesson 10 ADD Reflection 07.03 20:18

--- Lesson10/answer.sql
+++ Lesson10/answer.sql
@@ -1,7 +1,9 @@
 SELECT * FROM Customers
 LEFT JOIN Orders ON
-Orders.CustomerID = Customers.CustomerID AND
-Orders.CustomerID = NULL
+Orders.CustomerID = Customers.CustomerID 
+WHERE Orders.CustomerID is NULL
+
+Неправильно проверил отсутствие значения в FK таблицы Orders.
 
 SELECT 'Customer' As Type, ContactName, City, Country
 FROM Customers

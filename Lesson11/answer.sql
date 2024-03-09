01ad9c8f288639cad303067f52e817ed012637a7
Maxillas <maksanchutin@mail.ru>
2024-03-09 12:12:26 +0500

    Lesson 12 Done 9.03 12:12

---
+++ Lesson11/answer.sql
@@ -0,0 +1,9 @@
+INSERT INTO Employees (FirstName, LastName)
+VALUES ('Max', 'Anch')
+
+INSERT INTO EmployeeTerritories (EmployeeID, TerritoryID)
+VALUES ('10', '06897')
+
+INSERT INTO Orders (CustomerID)
+VALUES ('ANATR')
+/*Конфликтов не возникло, запись успешно добавилась

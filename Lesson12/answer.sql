73e5cf602f50cda7bb6d236be78754e1a31014b1
Maxillas <maksanchutin@mail.ru>
2024-03-12 19:01:25 +0500

    Lesson 12 Done, 12.03 19:01

---
+++ Lesson12/answer.sql
@@ -0,0 +1,16 @@
+UPDATE [Order Details]
+SET Discount = 0.2
+WHERE Quantity > 50
+
+UPDATE [northwind].[dbo].[Contacts] /*Work only that, "Contact" without full path does not work*/
+SET City = 'Piter', Country = 'Russia'
+WHERE City = 'Berlin', Country = 'Germany'
+
+INSERT INTO Shippers(CompanyName, Phone)
+VALUES ('Gazprom', '800-535-35-35'), 
+	   ('Lukoil', '800-111-35-35')
+
+DELETE FROM Shippers
+WHERE ShipperID > 3
+
+Критерий удаления - значение ID больше 3
